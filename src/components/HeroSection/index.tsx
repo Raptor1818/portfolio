404ce9cce98f9026868cd747c0e610f8ed998f71
Raptor <francyoppureno@gmail.com>
2025-12-15 17:53:42 +0100

    Base hero section

---
+++ src/components/HeroSection/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Button } from "@/components/ui/button";
+import { Download, ExternalLink } from "lucide-react";
+import { FaItchIo, FaGithub } from "react-icons/fa6";
+
+type Props = {}
+
+const index = (_props: Props) => {
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-9xl">Francesco Nisi</h1>
+      <h2>IT Diploma · Game Jams · Web Development</h2>
+      <div className="flex flex-row gap-2">
+        <Button variant="download">Resume <Download /></Button>
+        <Button variant="github"><FaGithub /> Github <ExternalLink /></Button>
+        <Button variant="itch"><FaItchIo />Itch.io <ExternalLink /></Button>
+      </div>
+    </div>
+  )
+}
+
+export default index
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,10 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+
+        download: "bg-white text-black border border-zinc-200 hover:bg-zinc-300",
+        github: "bg-[#24292e] text-white hover:bg-[#24292e]/70",
+        itch: "bg-[#FA5C5C] text-white hover:bg-[#FA5C5C]/70",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
