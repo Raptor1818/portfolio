8a61c719ab4bdedc9c1aa003862320655a6e4850
Raptor <francyoppureno@gmail.com>
2025-12-15 18:58:41 +0100

    WIP placement

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -8,13 +8,17 @@ type Props = {}
 
 const index = (_props: Props) => {
   return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-9xl">Francesco Nisi</h1>
-      <h2>🇮🇹 · Game Dev · Web Dev</h2>
-      <div className="flex flex-row gap-2">
-        <Button variant="download">Resume <Download /></Button>
-        <Button variant="github"><FaGithub /> Github <ExternalLink /></Button>
-        <Button variant="itch"><FaItchIo />Itch.io <ExternalLink /></Button>
+    <div className="w-full h-screen flex justify-center items-start mt-24">
+      <div className='h-2/5 flex flex-col justify-between items-start'>
+        <div className='flex flex-col gap-4'>
+          <h1 className="text-9xl">Francesco Nisi</h1>
+          <h2>🇮🇹 · Game Dev · Web Dev</h2>
+        </div>
+        <div className="flex flex-row gap-2">
+          <Button variant="download">Resume <Download /></Button>
+          <Button variant="github"><FaGithub /> Github <ExternalLink /></Button>
+          <Button variant="itch"><FaItchIo />Itch.io <ExternalLink /></Button>
+        </div>
       </div>
     </div>
   )
