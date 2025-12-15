c1babba2c570197ee6320b62b06add4eaa12132d
Raptor <francyoppureno@gmail.com>
2025-12-15 18:53:03 +0100

    Added allowedDevOrigins

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  allowedDevOrigins: ['devving.raptorino.com']
+};
 
 export default config;
