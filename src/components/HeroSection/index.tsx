09d3fade758a5c6cb52bdd07b51baf2a4a59eed6
Raptor <francyoppureno@gmail.com>
2025-12-16 22:57:33 +0100

    Updates
    Updated fonts, title, description, and text sizes

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -9,12 +9,12 @@ type Props = {}
 const index = (_props: Props) => {
   return (
     <div className="w-full h-screen flex justify-center items-start mt-24">
-      <div className='h-2/5 flex flex-col justify-between items-start'>
-        <div className='flex flex-col gap-4'>
-          <h1 className="text-9xl">Francesco Nisi</h1>
+      <div className='h-1/3 flex flex-col justify-between items-start'>
+        <div className='w-full flex flex-col gap-4 text-center md:text-start'>
+          <h1 className="text-4xl md:text-8xl font-gothic">Francesco Nisi</h1>
           <h2>🇮🇹 · Game Dev · Web Dev</h2>
         </div>
-        <div className="flex flex-row gap-2">
+        <div className="w-full flex flex-row justify-center gap-2">
           <Button variant="download">Resume <Download /></Button>
           <Button variant="github"><FaGithub /> Github <ExternalLink /></Button>
           <Button variant="itch"><FaItchIo />Itch.io <ExternalLink /></Button>
