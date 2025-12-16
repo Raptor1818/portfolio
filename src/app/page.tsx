3be74c18f17c0e51bc56e8d5782417adaac61f67
Raptor <francyoppureno@gmail.com>
2025-12-16 19:30:49 +0100

    Added navbar from indie UI
    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import HeroSection from "@/components/HeroSection";
+import { Navbar } from "@/components/Navbar";
 import ProjectSection from "@/components/ProjectSection";
-import { Card } from "@/components/ui/card";
 
 
 export default function HomePage() {
   return (
     <>
       <main className="flex flex-col gap-12 p-4 md:p-12">
+        <Navbar></Navbar>
         <HeroSection></HeroSection>
 
         <ProjectSection></ProjectSection>
